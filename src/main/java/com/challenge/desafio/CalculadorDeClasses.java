70ca897481d95b04cfcc0cfde5f793b06c4fde10
Giovane <giovane@MacBook-Pro-de-Giovane.local>
2019-10-28 00:01:43 -0300

    first commit

---
+++ src/main/java/com/challenge/desafio/CalculadorDeClasses.java
@@ -0,0 +1,27 @@
+package com.challenge.desafio;
+
+import java.math.BigDecimal;
+
+import com.challenge.interfaces.Calculavel;
+
+public class CalculadorDeClasses implements Calculavel {
+
+	@Override
+	public BigDecimal somar() {
+		
+		return null;
+	}
+
+	@Override
+	public BigDecimal subtrair() {
+		
+		return null;
+	}
+
+	@Override
+	public BigDecimal totalizar() {
+		
+		return null;
+	}
+
+}
---
+++ src/main/java/com/challenge/interfaces/Calculavel.java
@@ -0,0 +1,11 @@
+package com.challenge.interfaces;
+
+import java.math.BigDecimal;
+
+public interface Calculavel {
+
+	public BigDecimal somar();
+	public BigDecimal subtrair();
+	public BigDecimal totalizar();
+	
+}

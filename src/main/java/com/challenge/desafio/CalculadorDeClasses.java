e233f0491732b69d49f291c3f96dd99d98a57f67
Giovane <giovane@MacBook-Pro-de-Giovane.local>
2019-10-29 20:37:13 -0300

    implements challenge

--- src/main/java/com/challenge/desafio/CalculadorDeClasses.java
+++ src/main/java/com/challenge/desafio/CalculadorDeClasses.java
@@ -1,27 +1,38 @@
 package com.challenge.desafio;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
 
+import com.challenge.annotation.Somar;
+import com.challenge.annotation.Subtrair;
 import com.challenge.interfaces.Calculavel;
 
 public class CalculadorDeClasses implements Calculavel {
 
 	@Override
-	public BigDecimal somar() {
-		
-		return null;
+	public BigDecimal somar( Object classe ) throws IllegalArgumentException, IllegalAccessException {
+		return somarAtributosDeClasse( classe, Somar.class );
 	}
 
 	@Override
-	public BigDecimal subtrair() {
-		
-		return null;
+	public BigDecimal subtrair( Object classe ) throws IllegalArgumentException, IllegalAccessException {
+		return somarAtributosDeClasse( classe, Subtrair.class );
 	}
 
 	@Override
-	public BigDecimal totalizar() {
-		
-		return null;
+	public BigDecimal totalizar( Object classe ) throws IllegalArgumentException, IllegalAccessException {
+		return somar( classe ).subtract( subtrair( classe ) );
 	}
-
+	
+	private BigDecimal somarAtributosDeClasse( Object obj, Class<? extends Annotation> classe ) throws IllegalArgumentException, IllegalAccessException {
+		BigDecimal valor = BigDecimal.ZERO;
+		for( Field field : obj.getClass().getDeclaredFields() ) {
+			if( field.isAnnotationPresent( classe ) && field.getType().equals( BigDecimal.class ) ) {
+				field.setAccessible( Boolean.TRUE );
+				valor = valor.add( field.get( obj ) == null ? BigDecimal.ZERO : ( BigDecimal ) field.get( obj ) );
+			}
+		}
+		return valor;
+	}
 }
--- src/main/java/com/challenge/interfaces/Calculavel.java
+++ src/main/java/com/challenge/interfaces/Calculavel.java
@@ -3,9 +3,7 @@ package com.challenge.interfaces;
 import java.math.BigDecimal;
 
 public interface Calculavel {
-
-	public BigDecimal somar();
-	public BigDecimal subtrair();
-	public BigDecimal totalizar();
-	
+	BigDecimal somar( Object classe ) throws IllegalArgumentException, IllegalAccessException;
+	BigDecimal subtrair( Object classe ) throws IllegalArgumentException, IllegalAccessException;
+	BigDecimal totalizar( Object classe ) throws IllegalArgumentException, IllegalAccessException;
 }
